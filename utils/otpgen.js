e9465fede33cc631af8ae337ad900fc2c572d86b
Codehackerone <soumyajitdatta123@gmail.com>
2021-05-07 01:07:51 +0530

    otp generator added

---
+++ utils/otpgen.js
@@ -0,0 +1,11 @@
+const getRandomotp = (length) => {
+    var result = '';
+    var characters = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    var charactersLength = characters.length;
+    for (var i = 0; i < length; i++) {
+        result += characters.charAt(
+            Math.floor(Math.random() * charactersLength)
+        );
+    }
+    return result;
+};
