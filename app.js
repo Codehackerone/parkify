9763dc407926f8db638315f680ca9b370689ea55
Codehackerone <soumyajitdatta123@gmail.com>
2021-05-06 22:44:21 +0530

    basic structure added

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const favicon = require('serve-favicon');
+const session = require('express-session');
+const MongoDBStore = require('connect-mongo')(session);
+const flash = require('express-flash');
+const helmet = require('helmet');
+const methodOverride = require('method-override');
+
+require('dotenv').config();
+
+const port = Number(process.env.PORT);
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(methodOverride('_method'));
+app.use(helmet({ contentSecurityPolicy: false }));
+
+app.use(express.static(__dirname + '/public'));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.get('*', (req, res) => {
+    res.render('not-found');
+});
+
+
+app.listen(port, () =>
+    console.log(`Parkify running at http://localhost:${port}`)
+);
