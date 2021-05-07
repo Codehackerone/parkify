2e5a4069968a4b42270b782c1f7a1bd604096eb2
Codehackerone <soumyajitdatta123@gmail.com>
2021-05-07 09:42:50 +0530

    isverified added in middleware

--- middleware/login.middleware.js
+++ middleware/login.middleware.js
@@ -14,12 +14,22 @@ const IsLoggedInMiddleware = () => {
                 if (!decoded)
                     return res.status(401).json('Expired or Invalid token');
                 let user = await User.findOne({ username: decoded.username });
-                req.body.email = user.email;
-                req.body.name = user.name;
-                req.body.verified=user.verified;
-                req.body.phone=user.phone;
-                req.body.username = user.username;
-                next();
+                if (!user)
+                {
+                    res.send('User doesnt exist');
+                }
+                else if(user.verified===false)
+                {
+                    res.send('Not Verified. Verify First');
+                }
+                else{
+                    req.body.email = user.email;
+                    req.body.name = user.name;
+                    req.body.verified=user.verified;
+                    req.body.phone=user.phone;
+                    req.body.username = user.username;
+                    next();
+                }
             } catch (error) {
                 return res.status(500).send(error);
             }
