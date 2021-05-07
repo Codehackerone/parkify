61f9402c4f8f04fabb0bbe01019eabf287660e48
Codehackerone <soumyajitdatta123@gmail.com>
2021-05-07 19:47:39 +0530

    garage model added

---
+++ models/slot.model.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const slotSchema = new Schema(
+    {
+        garage_id: {
+            type: Schema.Types.ObjectId,
+            ref: 'Garage',
+        },
+        name: {
+            type: String,
+            required: true,
+        },
+        type: {
+            type:String,
+            required: ["Big", "Medium", "Small"]
+        },
+        price: {
+            type: Number,
+            required: true,
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+let Slot = mongoose.model('Slot', bookSchema);
+
+module.exports = Slot;
