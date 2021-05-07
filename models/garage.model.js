0fb6afd272f8592b2e2a5f843720f3821c303119
Codehackerone <soumyajitdatta123@gmail.com>
2021-05-07 19:38:33 +0530

    garage model added

---
+++ models/garage.model.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const garageSchema = new Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        location: {
+            type: String,
+            required: true,
+        },
+        location_cat:{
+            type:String,
+            required: ["outskirts", "prime", "nice"]
+        },
+        size: {
+            type: String,
+            required: true,
+        },
+        owner:{
+            type: String,
+            required: true,
+        }
+    },
+    {
+        timestamps: true,
+    }
+);
+
+let Garage = mongoose.model('Garage', garageSchema);
+
+module.exports = Garage;
