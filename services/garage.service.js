72d133f3b705f3ed3b2055a05c2448a4cff03318
Codehackerone <soumyajitdatta123@gmail.com>
2021-05-23 18:38:31 +0530

    distance calculator added

--- services/garage.service.js
+++ services/garage.service.js
@@ -47,11 +47,25 @@ const ReturnCoords=async()=>
   return coords;
 }
 
+const DistanceCal=(lat1,lat2, lon1, lon2)=>
+{   
+  lon1 = lon1 * Math.PI / 180;
+  lon2 = lon2 * Math.PI / 180;
+  lat1 = lat1 * Math.PI / 180;
+  lat2 = lat2 * Math.PI / 180;
+  let dlon = lon2 - lon1;
+  let dlat = lat2 - lat1;
+  let a=Math.pow(Math.sin(dlat / 2), 2)+ Math.cos(lat1) * Math.cos(lat2)* Math.pow(Math.sin(dlon / 2),2);
+  let c=2 * Math.asin(Math.sqrt(a));
+  let r=6371;
+  return(c * r);
+}
 
 module.exports = {
   AddGarage,
   FindGarage,
   AllGarages,
   DeleteGarage,
-  ReturnCoords
+  ReturnCoords,
+  DistanceCal
 };
